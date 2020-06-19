b4c4d0840f7b4e455f214592159f734dcff3e6c2
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-06-19 15:37:34 -0300

    Add files via upload

---
+++ gatos.js
@@ -0,0 +1,33 @@
+let angle = 0;
+let star;
+let data;
+
+function preload() {
+  data = loadJSON("stars_named.json");
+}
+
+function setup() {
+  star = data.star;
+  createCanvas(710, 400, WEBGL);
+  //console.log(data);
+}
+
+function draw() {
+  background(0);
+  normalMaterial();
+  translate(0, 0, 0);
+  angle -= 0.001
+
+  for (let i = 0; i < star.length - 1; i++) {
+    translate(0, 0, mouseY / 30);
+
+    push()
+    rotateY(angle);
+    translate(star[i].galX * 10, star[i].galY * 10, star[i].galZ * 10);
+
+    fill(star[i].color);
+    sphere(10 / (1 + star[i].dist/2));
+    pop();
+  }
+
+}
