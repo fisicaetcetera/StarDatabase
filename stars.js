b0e8206f59802456400ddcb2573bf1c69d78141a
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-06-19 10:33:32 -0300

    Update stars.js

--- stars.js
+++ stars.js
@@ -1,14 +1,24 @@
 let data;
+let star;
 function preload(){
   data = loadJSON("stars_named.json");
 }
 function setup() {
   createCanvas(400, 400); 
+  star = data.star;
+  for(i=0; i<10; i++){
+   console.log(star[i].starName, star[i].dist)
+    {
+  }
+  
   // console.log(data);
+  console.log(
 }
 function draw() {
   background(0);
   fill(data.star[1].color);
+   
+ 
   text(data.star[0].starName, 10,50);
   text(data.star[0].dist, 30,50);
   text(data.star[1].starName, 10,70);
