e0f8969f87f2e9972e31cb340e660d5e522a301a
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-06-19 08:36:21 -0300

    Update gatos.js

--- gatos.js
+++ gatos.js
@@ -4,13 +4,12 @@ function preload(){
 }
 function setup() {
   createCanvas(400, 400); 
-  console.log(data);
+  // console.log(data);
 }
 function draw() {
   background(0);
   fill(255,110,20);
-  text(data.star[0].nome, 10,50);
-  text(data.gato[1].nome, 10, 70);
-  text(data.gato[0].revolution, 10, 90);
-  text(data.gato[0].raiva, 10,110);
-}
+  text(data.star[0].starName, 10,50);
+  text(data.star[0].dist, 10,50);
+  text(data.star[1].dist, 10,50);
+  text(data.star[1].dist, 10,50);
