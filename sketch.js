30e044be66073a4f82294759738d232532956d17
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-09-27 15:20:28 -0300

    Add files via upload
    Now you travel through the stars, making jumps.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 // To include a camera
 // that flies through the stars
 // Can especify colors of stars or use the color from the data
-//
+// Each loop from the point of view of the next star.  Starts // with Sirius.
 let angle = 0;
 let star;
 let data;
@@ -9,6 +9,9 @@ let cor;
 let alfax, alfay, alfaz, alfadist;
 let starx, stary, starz, stardist;
 let radiusSun = 3.5;
+let estrela = 9;
+let f;
+let myFont;
 
 function preload() {
   data = loadJSON("stars_named.json");
@@ -16,85 +19,74 @@ function preload() {
 
 function setup() {
   star = data.star;
-  createCanvas(1366,768, WEBGL);
+  createP("Star datbase by Jay Crossler");
+  createP("js code by Enivaldo Bonelli");
+  createP("Instructions below");
+  createCanvas(600, 400, WEBGL);
   alfax = star[9].galX;
   alfay = star[9].galY;
   alfaz = star[9].galZ;
-  alfadist = star[9].dist;
+  //botão para controla saltos
+  jump = createButton("Jump!");
+  jump.mousePressed(Jump);
+  h3 = createElement('h5', 'Estrela perto de nós');
+  h3.html(star[estrela].starName);
+  createP("Instructions: you travel through the galaxy.  When you click 'Jump', you jump through hyperspace to another star, name shown.")
+  createP("When your ship passes by the star, the camera will reverse to show it again.");
 
-  for (let is = 0; is < star.length; is++) {
-    console.log(is, star[is].starName, star[is].dist);
-  }
+  //lista estrelas
+  //  for (let is = 0; is < star.length; is++) {
+  //    console.log(is, star[is].starName, star[is].dist);
+  //  }
+  estrela = floor(random(star.length - 1));
 }
+
 
 function draw() {
   background(0);
   normalMaterial();
+  h3.html(star[estrela].starName);
+
   //translate(0, 0, 0);
-  angle -= 0.01
-  let fc = 0.01;
-  //camera(sin(frameCount * fc) * 100, cos(frameCount * fc) * 100, sin(frameCount * fc) * 1000, 0,0,0, 0, 1, 0);
-  camera(0,0,1000*sin(frameCount*fc), sin(100+frameCount*fc),100,100+cos(frameCount*fc),0,1,0);
+  angle -= 0.005
+  let fc = 0.001;
+  //camera(sin(frameCount * fc) * 50, cos(frameCount * fc) * 50, sin(frameCount * fc) * 50, 0,0,0, 0, 1, 0);
+  camera(0, 0, 250 * sin(frameCount * fc*3), 0, 0,0,0, 1, 0);
 
   //"Sirius", "id":601, "galX":-5.895, "galY":-6.152, "galZ":-1.167,  "dist":8.6, "color":"#baccff"}, 
 
 
-  for (let i = 0; i < star.length - 1; i++) {
+
+  for (let i = 0; i < star.length - 1 && i != estrela; i++) {
     //translate(0, 0, mouseY / 30);
     //beginShape();
     radius = radiusSun;
-    translate(0, 0, 0)
+
+
     push()
+    translate(0, 0, 0)
     rotateY(angle);
-    starx = star[i].galX * 30 - alfax * 30;
-    stary = star[i].galY * 30 - alfay * 30;
-    starz = star[i].galZ * 30 - alfaz * 30;
+    starx = star[i].galX * 30 - star[estrela].galX * 30;
+    stary = star[i].galY * 30 - star[estrela].galY * 30;
+    starz = star[i].galZ * 30 - star[estrela].galZ * 30;
+    alfadist = star[estrela].dist;
+    cor = star[estrela].color;
 
     translate(starx, stary, starz);
     distancia = abs(star[i].dist - alfadist);
-    if (distancia < 9) {
-      cor = "#0000FF";
-      //console.log(star.starName);
-    } else if (star[i].dist == 0) {
-      cor = "FFFFFF";
-      //console.log(star[i].starName);
-    } else {
-      cor = "#FF0000";
-    }
-
-    if (star[i].dist < 4.7) {
-      cor = "#00f0ff";
-      radius = radiusSun;
-      //proxima centauri
-    }
-    if (star[i].dist == 0) {
-      cor = "#ffffff";
-      //radius = 3.5;
-    }
-
-    if (star[i].id == 601) {
-      cor = "#baccff";
-      radius = 2*radiusSun;
-    }
-
-    if (star[i].id == 221201) {
-      //Betegeuse
-      cor = "#ffc766";
-      radius = 1.5*radiusSun;
-    }
-
 
     fill(cor);
-    sphere(radius / (0.5 + distancia / 2));
+    sphere(radius / (0.5 + distancia / 8));
     pop();
   }
 }
 
 
-function mousePressed() {
-  noLoop();
+function Jump() {
+  estrela = floor(random(star.length - 1));
+  redraw();
 }
 
-function mouseReleased() {
-  loop();
-}
+//function mouseReleased() {
+//  loop();
+//}
